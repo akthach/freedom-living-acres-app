3e633ec93ee94b3b2aaa3f3f75fcbd126bd9ef25
Jimmy Tran <qtran11@masonlive.gmu.edu>
2020-01-29 21:08:22 -0500

    chore(build): initialize Spring Boot app

---
+++ src/main/java/com/djt/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.djt.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import static java.lang.String.format;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+    @GetMapping(value = "/{personName}")
+    public String greet(@PathVariable(value = "name") String name) {
+        return format("Hello %s!", name);
+    }
+
+}
